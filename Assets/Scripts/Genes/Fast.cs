7643135372c5bee7764f75fa52e9fdaf988ba685
Andrew <andrew.ramos92@gmail.com>
2016-10-01 16:29:05 -0400

    nerfed Fast gene

--- Assets/Scripts/Genes/Fast.cs
+++ Assets/Scripts/Genes/Fast.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts.Genes {
         float moveSpeed;
 
         public Fast(Organism org, float magnitude) {
-            moveSpeed = org.genetics.moveSpeed.absDiff(Mathf.Max(0.3f, org.genetics.moveSpeed * Random.Range(1f, 1.02f)) * magnitude);
+            moveSpeed = org.genetics.moveSpeed.absDiff(Mathf.Max(0.15f, org.genetics.moveSpeed * Random.Range(1f, 1.03f)) * magnitude);
             apply = () => {
                 org.genetics.moveSpeed += moveSpeed;
             };
